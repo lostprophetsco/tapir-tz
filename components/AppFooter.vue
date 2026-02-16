fbc84dfeffc950007734db1cc2e1e69328af1d8c
Vadim Zavgorodniy <lostprophetsco@gmail.com>
2026-02-16 17:05:40 +0300

    feat: add AppFooter component

---
+++ components/AppFooter.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts"></script>
+<template>
+  <footer class="footer">
+    <div class="container footer__container">
+      <nav class="footer__menu">
+        <ul class="footer__menu-list">
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Каталог</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Все бренды</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Nike</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Adidas</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Puma</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">New Balance</a>
+          </li>
+        </ul>
+      </nav>
+
+      <nav class="footer__menu">
+        <ul class="footer__menu-list">
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Помощь</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Доставка</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Заказ и оплата</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Обмен и Возврат</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">F. A. Q.</a>
+          </li>
+        </ul>
+      </nav>
+
+      <nav class="footer__menu">
+        <ul class="footer__menu-list">
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Информация</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Преимущества</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Гарантия и безопасность</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Индивидуальный заказ</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Акции</a>
+          </li>
+        </ul>
+      </nav>
+
+      <nav class="footer__menu">
+        <ul class="footer__menu-list">
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Компания</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">О нас</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Блог</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Отзывы</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">Контакты</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -10,9 +10,12 @@
       </div>
     </main>
   </div>
+
+  <AppFooter />
 </template>
 
 <script setup lang="ts">
 import AppHeader from '~/components/AppHeader.vue'
 import AppButton from '~/components/AppButton.vue'
+import AppFooter from '~/components/AppFooter.vue'
 </script>
